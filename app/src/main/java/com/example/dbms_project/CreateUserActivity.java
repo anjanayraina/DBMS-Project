8dad547d46c81039405428c97e1aa0e0715f1325
Anjanay Raina <anjanayraina326@gmail.com>
2022-04-16 18:11:33 +0530

    CardView Created

--- app/src/main/java/com/example/dbms_project/CreateUserActivity.java
+++ app/src/main/java/com/example/dbms_project/CreateUserActivity.java
@@ -2,7 +2,10 @@ package com.example.dbms_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateUserActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class CreateUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
